b70ae48b2f615e30a9c79a2b81d319c0bff728a6
avidalh <avidalh@gmail.com>
2015-06-30 20:41:31 +0100

    mbp commit:
    Working on README.md file...
    This commit is just to see how it looks... ;-)

--- application.py
+++ application.py
@@ -742,5 +742,5 @@ def make_session_permanent():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8080)
+    app.run(host = '0.0.0.0', port = 5050)
 
